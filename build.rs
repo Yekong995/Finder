6f42099a594878895999edf2e11b3d694a8f3604
Yekong995 <yekongyt@gmail.com>
2025-10-19 23:49:20 +0800

    For slint ui generate code

---
+++ build.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let enabled = std::env::var("CARGO_FEATURE_UI").is_ok();
+
+    if enabled {
+        compile_slint();
+    }
+}
+
+#[cfg(feature = "ui")]
+fn compile_slint() {
+    slint_build::compile("ui/main.slint").unwrap();
+}
+
+// 防止未启用 feature 时编译报错
+#[cfg(not(feature = "ui"))]
+fn compile_slint() {
+    return;
+}
